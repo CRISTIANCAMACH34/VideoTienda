8dc8592a47b8ad83aaae7b9c040b3d1a6f055ff0
Jeferson Alvarez <mymotre213@gmail.com>
2025-06-11 18:00:18 -0500

    Inicializando la clase copia

---
+++ source/uniandes/cupi2/videotienda/mundo/Copia.java
@@ -0,0 +1,13 @@
+package uniandes.cupi2.videotienda.mundo;
+
+public class Copia {
+	
+	private String tituloPelicula;
+	
+	private int codigo;
+
+	public Copia(String laPelicula, int elCodigo) {
+		tituloPelicula = laPelicula;
+		codigo = elCodigo;
+	}
+}
